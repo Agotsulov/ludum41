a501cc200a93830298132b07202a812a759e1818
byzilio <agotsulov@gmail.com>
2018-04-20 13:49:45 +0600

    Create project

---
+++ desktop/src/com/lce/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.lce.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.lce.LCE;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LCE(), config);
+	}
+}
